f73b791b7a8bb8104a22064803dd4dfe672ad09b
Phean <phean.p07@gmail.com>
2020-06-23 23:22:35 +0700

    First push

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const app=express();
-
+const PORT=process.env.PORT || 5000;
 app.use(express.json());
 
 app.use('/api/user',require('./route/user_login'));
 app.use('/api/student',require('./route/student'));
 
-app.listen(5000,()=>console.log(`Server start at 5000`));
+app.listen(PORT,()=>console.log(`Server start at 5000`));
