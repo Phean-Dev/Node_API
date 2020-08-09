700fbed965396031507099c4a4cfcc19fa218ef3
Phean <phean.p07@gmail.com>
2020-08-09 15:29:14 +0700

    Add cors

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require('express');
 const bodyParser = require('body-parser');
 const app=express();
 const PORT=process.env.PORT || 5000;
+const cors=require('cors');
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
